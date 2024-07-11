cfacf6033b57f3dffb9f03172eb6215ab4505707
Lucas Mösch <lucasmoesch@live.de>
2024-07-12 00:29:31 +0200

    Added rpc module

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RpcModule } from './core/rpc/rpc-module';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const rpcModule = new RpcModule();
+
+  app.enableCors();
+  app.useWebSocketAdapter(new WsAdapter(app));
+
+  rpcModule.register(app["container"]);
+  
   await app.listen(3900);
 }
 bootstrap();
